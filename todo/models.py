7bbc25790bce2f5a20642aeaf0c0b1a95e20401b
rovan89 <devireland89@gmail.com>
2022-03-30 13:39:21 +0000

    Fix bugs

--- todo/models.py
+++ todo/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
  
 class Item(models.Model):
-    name = models.Charfield(max_lenght=50, null=False, blank=False)
-    done = models.Booleanfield(null=False, blank=False, default=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
         return self.name
