3987f753d8d9e6e1c94f69e519bd7d6dac9cacd4
rovan89 <devireland89@gmail.com>
2022-04-04 20:13:00 +0000

    Break

--- todo/test_views.py
+++ todo/test_views.py
@@ -16,12 +16,24 @@ class TestViews(TestCase):
 
     def test_get_edit_item_page(page):
         item = Item.objects.create(name= 'Test Todo Item')
-        response = self.client.get('/edit/99')
+        response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
     def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'Test Added Item'})
+        self.assertRedirects(response, '/')
 
     def test_can_delete_item(self):
+        item = Item.objects.create(name= 'Test Todo Item')
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_item = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_item), 0)
 
     def test_can_toggle_item(self):
+        item = Item.objects.create(name= 'Test Todo Item', done=True)
+        response = self.client.get(f'/toggle/{item.id}')
+        self.assertRedirects(response, '/') 
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.done)
